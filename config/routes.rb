242c4bc18541db4cf8d4dd2aa50a31c1e2e59d9d
yutakahashi1 <federeryuukun1@gmail.com>
2020-03-20 20:10:19 +0900

    変更しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'messages/index'
+
+  Rails.application.routes.draw do
+  get 'messages/index'
+
+    root "messages#index"
+  end
 end
