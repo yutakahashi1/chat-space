aca36499d37299fa00589175dc91ad2510cc5a73
yutakahashi1 <federeryuukun1@gmail.com>
2020-03-23 11:22:38 +0900

    グループ機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  validates :name, presence: true, uniqueness: true
- end
+       # Include default devise modules. Others available are:
+       # :confirmable, :lockable, :timeoutable and :omniauthable
+       devise :database_authenticatable, :registerable,
+              :recoverable, :rememberable,  :validatable
+     
+       has_many :group_users
+       has_many :groups, through: :group_users
+     end
