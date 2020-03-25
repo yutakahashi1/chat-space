5cb23fa4cdee4370ac40c5d2cd5336645f3a0eb3
yutakahashi1 <federeryuukun1@gmail.com>
2020-03-25 12:07:09 +0900

    自動更新機能実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,61 +1,102 @@
 $(function(){ 
-  function buildHTML(message){
-   if ( message.image ) {
-     var html =
-      `<div class="message">
-         <div class="message__upper-info">
-           <div class="message__upper-info__talker">
-             ${message.user_name}
-           </div>
-           <div class="message__upper-info__date">
-             ${message.created_at}
-           </div>
-         </div>
-         <div class="message__comment">
-          ${message.content}
-         </div>
-         <img src=${message.image} >
-       </div>`
-     return html;
-   } else {
-     var html =
-      `<div class="message">
-         <div class="message__upper-info">
-           <div class="message__upper-info__talker">
-             ${message.user_name}
-           </div>
-           <div class="message__upper-info__date">
-             ${message.created_at}
-           </div>
-         </div>
-         <div class="lower-message">
-          ${message.content}
-         </div>
-       </div>`
-     return html;
-   };
- }
-$('#new_message').on('submit', function(e){
- e.preventDefault();
- var formData = new FormData(this);
- var url = $(this).attr('action')
- $.ajax({
-   url: url,
-   type: "POST",
-   data: formData,
-   dataType: 'json',
-   processData: false,
-   contentType: false
- })
-  .done(function(data){
-    var html = buildHTML(data);
-    $('.main-messages').append(html);      
-    $('form')[0].reset();
-    $('.main-messages').animate({ scrollTop: $('.main-messages')[0].scrollHeight});
-    $('.form__submit').prop('disabled', false);
-  })
-  .fail(function() {
-    alert("メッセージ送信に失敗しました");
-});
-})
+  console.log("done")
+  var buildHTML = function(message) {
+    if (message.content && message.image) {
+      var html = `<div class="message" data-message-id=` + message.id + `>` +
+        `<div class="message__upper-info">` +
+          `<div class="message__upper-info__talker">` +
+            message.user_name +
+          `</div>` +
+          `<div class="message__upper-info__date">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="message__comment">` +
+          message.content +
+          `<img src="` + message.image + `" class="lower-message__image" >` +
+        `</div>` +
+      `</div>`
+    } else if (message.content) {
+      var html = `<div class="message" data-message-id=` + message.id + `>` +
+        `<div class="message__upper-info">` +
+          `<div class="message__upper-info__talker">` +
+            message.user_name +
+          `</div>` +
+          `<div class="message__upper-info__date">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="message__comment">` +
+          message.content +
+        `</div>` +
+      `</div>`
+    } else if (message.image) {
+      var html = `<div class="message" data-message-id=` + message.id + `>` +
+        `<div class="message__upper-info">` +
+          `<div class="message__upper-info__talker">` +
+            message.user_name +
+          `</div>` +
+          `<div class="message__upper-info__date">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="message__comment">` +
+          `<img src="` + message.image + `" class="lower-message__image" >` +
+        `</div>` +
+      `</div>`
+    };
+    return html;
+  };
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+     .done(function(data){
+       var html = buildHTML(data);
+       $('.main-messages').append(html);      
+       $('form')[0].reset();
+       $('.main-messages').animate({ scrollTop: $('.main-messages')[0].scrollHeight});
+       $('.form__submit').prop('disabled', false);
+     })
+     .fail(function() {
+       alert("メッセージ送信に失敗しました");
+   });
+  });
+  var reloadMessages = function() {
+    var last_message_id = $('.message:last').data("message-id");
+    console.log(last_message_id)
+
+
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.main-messages').append(insertHTML);
+        $('.main-messages').animate({ scrollTop: $('.main-messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+    if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+      console.log("done")
+      setInterval(reloadMessages, 7000);
+    }
 });
