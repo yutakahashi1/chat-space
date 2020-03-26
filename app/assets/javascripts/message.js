9b3a9891fa53f18733fa9f5c81589a70d18b3dcc
yutakahashi1 <federeryuukun1@gmail.com>
2020-03-26 17:15:05 +0900

    ！！自動更新機能再修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,65 +1,3 @@
-// $(function(){ 
-//   function buildHTML(message){
-//    if ( message.image ) {
-//      var html =
-//       `<div class="message">
-//          <div class="message__upper-info">
-//            <div class="message__upper-info__talker">
-//              ${message.user_name}
-//            </div>
-//            <div class="message__upper-info__date">
-//              ${message.created_at}
-//            </div>
-//          </div>
-//          <div class="message__comment">
-//           ${message.content}
-//          </div>
-//          <img src=${message.image} >
-//        </div>`
-//      return html;
-//    } else {
-//      var html =
-//       `<div class="message">
-//          <div class="message__upper-info">
-//            <div class="message__upper-info__talker">
-//              ${message.user_name}
-//            </div>
-//            <div class="message__upper-info__date">
-//              ${message.created_at}
-//            </div>
-//          </div>
-//          <div class="lower-message">
-//           ${message.content}
-//          </div>
-//        </div>`
-//      return html;
-//    };
-//  }
-// $('#new_message').on('submit', function(e){
-//  e.preventDefault();
-//  var formData = new FormData(this);
-//  var url = $(this).attr('action')
-//  $.ajax({
-//    url: url,
-//    type: "POST",
-//    data: formData,
-//    dataType: 'json',
-//    processData: false,
-//    contentType: false
-//  })
-//   .done(function(data){
-//     var html = buildHTML(data);
-//     $('.main-messages').append(html);      
-//     $('form')[0].reset();
-//     $('.main-messages').animate({ scrollTop: $('.main-messages')[0].scrollHeight});
-//     $('.form__submit').prop('disabled', false);
-//   })
-//   .fail(function() {
-//     alert("メッセージ送信に失敗しました");
-// });
-// })
-// });
-
 $(function(){ 
   var buildHTML = function(message) {
     if (message.content && message.image) {
@@ -133,7 +71,6 @@ $(function(){
   });
   var reloadMessages = function() {
     var last_message_id = $('.message:last').data("message-id");
-    console.log(last_message_id)
     $.ajax({
       url: "api/messages",
       type: 'get',
